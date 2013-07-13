2a7f918ad392fb67c6a8c71082007833273cbf3d
Joel Latino <jlatino@sapo.pt>
2013-07-13 23:14:41 +0100

    Rename to CDAjs

--- api-0.0.1/api.js
+++ api-0.0.1/api.js
@@ -5,13 +5,13 @@ YUI.add("yuidoc-meta", function(Y) {
         "CDA.Exception"
     ],
     "modules": [
-        "CDA.js"
+        "CDAjs"
     ],
     "allModules": [
         {
-            "displayName": "CDA.js",
-            "name": "CDA.js",
-            "description": "This is <b>CDA.js</b> - a stand-alone javascript library for working with Pentaho plugin\n\"Community Data Access\" CDA (<i>Community Data Access</i>) is a open source plugin for\n<b>Pentaho Business Analysis</b> suite, designed to allow greater flexibility for data sources.\nPowered by <a href=\"http://www.webdetails.pt/ctools/cda.html\" target=\"_blank\">WebDetails</a>.\n<br/><br/> <b>CDA.js</b> is cross-browser and <a\nhref=\"http://nodejs.org\"target=\"_blank\">Node.js</a> compatible and enables web-browser-based\nanalytical Pentaho Business Intelligence applications."
+            "displayName": "CDAjs",
+            "name": "CDAjs",
+            "description": "This is <b>CDAjs</b> - a stand-alone javascript library for working with Pentaho plugin\n\"Community Data Access\" CDA (<i>Community Data Access</i>) is a open source plugin for\n<b>Pentaho Business Analysis</b> suite, designed to allow greater flexibility for data sources.\nPowered by <a href=\"http://www.webdetails.pt/ctools/cda.html\" target=\"_blank\">WebDetails</a>.\n<br/><br/> <b>CDAjs</b> is cross-browser and <a\nhref=\"http://nodejs.org\"target=\"_blank\">Node.js</a> compatible and enables web-browser-based\nanalytical Pentaho Business Intelligence applications."
         }
     ]
 } };
