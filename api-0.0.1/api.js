f9b02cdf36ecd85644b10cf31e6aa9add2d9118e
Joel Latino <jlatino@sapo.pt>
2013-07-13 18:59:04 +0100

    Initial commit of CDA.js website

---
+++ api-0.0.1/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "CDA",
+        "CDA.Exception"
+    ],
+    "modules": [
+        "CDA.js"
+    ],
+    "allModules": [
+        {
+            "displayName": "CDA.js",
+            "name": "CDA.js",
+            "description": "This is <b>CDA.js</b> - a stand-alone javascript library for working with Pentaho plugin\n\"Community Data Access\" CDA (<i>Community Data Access</i>) is a open source plugin for\n<b>Pentaho Business Analysis</b> suite, designed to allow greater flexibility for data sources.\nPowered by <a href=\"http://www.webdetails.pt/ctools/cda.html\" target=\"_blank\">WebDetails</a>.\n<br/><br/> <b>CDA.js</b> is cross-browser and <a\nhref=\"http://nodejs.org\"target=\"_blank\">Node.js</a> compatible and enables web-browser-based\nanalytical Pentaho Business Intelligence applications."
+        }
+    ]
+} };
+});
---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+jQuery(function() {
+	
+	// Clear form
+	jQuery('.button.form-button-reset').click(function(e) {
+		jQuery('name').val("");
+		jQuery('email').val("");
+		jQuery('subject').val("");
+		jQuery('message').val("");
+	});
+	
+	//Submit form
+	
+});
