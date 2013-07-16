f08908bba70a9e426c96d7b95bd102e3c4e3b935
Joel Latino <jlatino@sapo.pt>
2013-07-16 19:25:56 +0100

    first source commit

---
+++ examples/expressjs-app-sample/routes/index.js
@@ -0,0 +1,31 @@
+
+/*
+ * GET home page.
+ */
+
+var cda = require("cda.js");
+
+exports.index = function(req, res) {
+  var obj = new cda.CDA({
+    url: "http://localhost:8080/pentaho/content/cda/",
+    username: "joe",
+    password: "password",
+    error: function(error) {
+      console.log("Error: " + error);
+      return;
+    }
+  });
+  obj.doQuery(function(xhr) {
+    res.send("Result Set: " + xhr.resultset);
+  }, {
+    params: {
+      dataAccessId: 1,
+      outputIndexId: 1,
+      pageSize: 0,
+      pageStart: 0,
+      path: "%2Fplugin-samples%2Fcda%2Fcdafiles%2Fscripting.cda",
+      sortBy: ""
+    }
+  });
+
+};
